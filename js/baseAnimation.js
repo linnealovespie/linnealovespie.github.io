bb975878759cf666d6d03380ef88c8208ce15d31
Linnea May <linnealovespie@gmail.com>
2016-07-16 20:26:53 -0500

    Finished about page

--- js/baseAnimation.js
+++ js/baseAnimation.js
@@ -2,12 +2,15 @@ $(document).ready(function () {
     $('.parallax').parallax();
 });
 
-var height = screen.availHeight;
+//sets the pages to the height of the scree
+var height = $(window).height() - 64;
 $(".parallax-container").css('height', height);
+$(".page2-container").css('min-height', height);
+//$(".about-container").css('min-height', height * .66);
 
-$(".scroll-icon").click(function () {
-    $(window).scrollTo(".hello-container");
-});
+//centers the "call to action" on the first page
+var callContainer = (height - $(".call-container").height() - 64) / 2;
+$(".call-container").css('margin-top', callContainer);
 
 var containerHeight = screen.getHeight()
 $(".page2-container").css('height', height / 2);
