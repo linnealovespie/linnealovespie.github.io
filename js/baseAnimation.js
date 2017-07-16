310fbfef6ee142da1fb7b5278293e434d5e0ffa8
Linnea May <linnealovespie@gmail.com>
2017-07-15 20:51:09 -0500

    Experience Page
    Yeah that blog ain't happening any time soon.

--- js/baseAnimation.js
+++ js/baseAnimation.js
@@ -5,13 +5,16 @@ $(document).ready(function () {
 
 //sets the pages to the height of the scree
 var height = $(window).height() - 64;
-$(".parallax-container").css('height', height);
-$(".page2-container").css('min-height', height);
+$("#landing-parallax").css('height', height);
+$("#section-parallax").css('height', height/2);
+
+//$(".page-container").css('min-height', height);
 //$(".about-container").css('min-height', height * .66);
 
 //centers the "call to action" on the first page
 var callContainer = (height - $(".call-container").height() - 64) / 2;
 $(".call-container").css('margin-top', callContainer);
+$(".section-container").css('margin-top', callContainer/1.5);
 
 var containerHeight = screen.getHeight()
 $(".page2-container").css('height', height / 2);
