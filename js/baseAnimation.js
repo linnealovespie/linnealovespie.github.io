f30a1414d547744784a9f850cdb5d6163cb5d950
Linnea May <linnealovespie@gmail.com>
2017-07-27 16:00:57 -0500

    Education section

--- js/baseAnimation.js
+++ js/baseAnimation.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     $('.parallax').parallax();
      $(".button-collapse").sideNav();
+    $('.tooltipped').tooltip({html: true});
 });
 
 //sets the pages to the height of the scree
 var height = $(window).height() - 64;
 $("#landing-parallax").css('height', height);
 $("#section-parallax").css('height', height/2);
+$("#section-parallax1").css('height', height/2);
 
 //$(".page-container").css('min-height', height);
 //$(".about-container").css('min-height', height * .66);
